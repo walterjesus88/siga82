c6c633603e1220b18f2792ca4a00e9b4a03f30d6
anddes <anddes@TRA.(none)>
2015-04-28 16:53:50 -0500

    listar, nuevo y ver propuesta

--- app/modules/propuesta/controllers/IndexController.php
+++ app/modules/propuesta/controllers/IndexController.php
@@ -11,9 +11,30 @@ class Propuesta_IndexController extends Zend_Controller_Action {
     }
     
     public function indexAction() {
-        echo "aaaaaa";
+        echo "waaaaaa";
             
     }
+
+    public function listarAction() {
+
+         $listapropuesta = new Admin_Model_DbTable_Propuesta();
+        $lista=$listapropuesta->_getPropuestaAll();
+       //print_r($listapropuesta);
+        $this->view->listapropuesta = $lista; 
+            
+    }
+
+    public function verAction() {
+        $codigo=$this->_getParam('codigo');
+        $propuestaid=$this->_getParam('propuestaid');
+        $revision=$this->_getParam('revision');
+
+        $buscapropuesta = new Admin_Model_DbTable_Propuesta();
+        $busca=$buscapropuesta->_getPropuestaxIndices($codigo,$propuestaid,$revision);
+        //print_r($listapropuesta);
+        $this->view->buscapropuesta = $busca; 
+            
+    }  
 	
 
     
