6f4df34923159e1cf94d4d10c46bdaae28385b4b
Nilton <niltonmb7@gmail.com>
2015-05-07 12:52:06 -0500

    avance timesheet

--- app/modules/timesheet/controllers/IndexController.php
+++ app/modules/timesheet/controllers/IndexController.php
@@ -3,18 +3,36 @@
 class Timesheet_IndexController extends Zend_Controller_Action {
 
     public function init() {
-    	$options = array(
+        $sesion  = Zend_Auth::getInstance();
+        if(!$sesion->hasIdentity() ){
+            $this->_helper->redirector('index',"index",'default');
+        }
+        $login = $sesion->getStorage()->read();
+        $this->sesion = $login; 
+        $options = array(
             'layout' => 'layout',
         );
         Zend_Layout::startMvc($options);
-
     }
     
     public function indexAction() {
         
     }
 
-    public function registroAction(){
+    public function calendarAction(){
         
+    }
+
+    public function registroAction(){
+        $this->_helper->layout()->disableLayout();
+        $fecha_inicio = $this->_getParam('fecha');
+        $fecha_inicio = date("d-m-Y", strtotime($fecha_inicio));
+        $this->view->fecha = $fecha_inicio;
+        $uid = $this->sesion->uid;
+        $dni = $this->sesion->dni;
+        $where["uid"] = $uid;
+        $tareo = new Admin_Model_DbTable_Tareo();
+        $data_tareo = $tareo->_getTareoXUid($where);
+        $this->view->actividades = $data_tareo;
     }
 }
