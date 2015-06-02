8afd202becb9b01b4ca5baeed66d71620e6d9cef
wjesus88 <wjesus88@gmail.com>
2015-06-02 06:58:36 -0800

    activar

--- app/modules/proyecto/controllers/IndexController.php
+++ app/modules/proyecto/controllers/IndexController.php
@@ -975,42 +975,6 @@ for ($j = 8; $j <= $data->sheets[0]['numCols']-1; $j++) {
     }
   }
 
-public function asignaractividadAction(){
-  try
-  {
-
-    //$proyectoid= $this->_getParam("proyectoid");
-    //$codigo_prop_proy= $this->_getParam("codigo_prop_proy");
-
-    $proyectoid= '1208.10.08';
-    $codigo_prop_proy= '13.10.022-1208.10.08-C';
-
-    $where = array( 'proyectoid' => $proyectoid,'codigo_prop_proy'=>$codigo_prop_proy,'estado' =>'P','isproyecto'=>'S');
-    $veract = new Admin_Model_DbTable_Actividad();
-    $viewactivity=$veract->_getFilter($where);
-    //print_r($viewactivity);
-    $this->view->actividades = $viewactivity;
-
-    $wherekip = array( 'proyectoid' => $proyectoid,'codigo_prop_proy'=>$codigo_prop_proy,'estado' =>'A');    
-    $verequipo= new Admin_Model_DbTable_Equipo();
-    $viewequipo=$verequipo->_getFilter($wherekip);
-    print_r($viewequipo);
-    $this->view->equipo = $viewequipo;
-
-    $area = new Admin_Model_DbTable_Area();
-    $area_view = $area->_getAreaAll();
-    $this->view->area = $area_view;
-
-
-  }
-  catch (Exception $e) {
-    print "Error: ".$e->getMessage();
-  }
-}
-
-
-
-
 public function subirpropuestaAction(){
   try {
     $proyectoid= $this->_getParam("proyectoid");
@@ -1554,6 +1518,67 @@ public function cargartarea2Action() {
     $dbequipo = new Admin_Model_DbTable_Equipo();
     $dbequipo->_save($dataequipo);
     
+  }
+
+  public function asignaractividadAction(){
+    try
+    {
+
+      //$proyectoid= $this->_getParam("proyectoid");
+      //$codigo_prop_proy= $this->_getParam("codigo_prop_proy");
+
+      $proyectoid= '1208.10.08';
+      $codigo_prop_proy= '13.10.022-1208.10.08-C';
+
+      $where = array( 'proyectoid' => $proyectoid,'codigo_prop_proy'=>$codigo_prop_proy,'estado' =>'P','isproyecto'=>'S');
+      $veract = new Admin_Model_DbTable_Actividad();
+      $viewactivity=$veract->_getFilter($where);
+      //print_r($viewactivity);
+      $this->view->actividades = $viewactivity;
+
+      $wherekip = array( 'proyectoid' => $proyectoid,'codigo_prop_proy'=>$codigo_prop_proy,'estado' =>'A');    
+      $verequipo= new Admin_Model_DbTable_Equipo();
+      $viewequipo=$verequipo->_getFilter($wherekip);
+      print_r($viewequipo);
+      $this->view->equipo = $viewequipo;
+
+      $area = new Admin_Model_DbTable_Area();
+      $area_view = $area->_getAreaAll();
+      $this->view->area = $area_view;
+
+
+    }
+    catch (Exception $e) {
+      print "Error: ".$e->getMessage();
+    }
+  }
+
+
+
+  public function agregaactividadAction(){
+    try
+    {
+  
+      $cargo= $this->_getParam("cargo");
+      $areaid= $this->_getParam("areaid");
+      $uid= $this->_getParam("uid");
+      $dni= $this->_getParam("dni");
+      $proyectoid= $this->_getParam("proyectoid");
+      $categoriaid= $this->_getParam("categoriaid");
+      $areaid= $this->_getParam("areaid");
+      $cargo= $this->_getParam("cargo");
+      $actividadid= $this->_getParam("actividadid");
+      $revision= $this->_getParam("revision");
+      $codigo_actividad= $this->_getParam("codigo_actividad");
+      $codigo_prop_proy= $this->_getParam("codigo_prop_proy");
+
+
+     } 
+
+      catch (Exception $e) {
+      print "Error: ".$e->getMessage();
+    }
+
   }
 
 }
