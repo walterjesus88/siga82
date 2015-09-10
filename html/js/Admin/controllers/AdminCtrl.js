e26ea571446bdf7e41e38144568954da4f9c36c8
dabesac <d.benito666@gmail.com>
2015-09-10 18:09:43 -0500

    estado usuario

--- html/js/Admin/controllers/AdminCtrl.js
+++ html/js/Admin/controllers/AdminCtrl.js
@@ -1,20 +1,43 @@
 app.controller('AdminCtrl', ['$scope','httpFactory', 'adminFactory',
     function($scope,httpFactory, adminFactory) {
-    var va = this;
-    //va.proyectos = [];
-    //va.control_documentario = [];
-    //va.variable=[{nombre:"jesus",texto:"ffff"},{nombre:"walter",texto:"sssss"}];
-    //$scope.name = 'Pablo';
+        var va = this;
+        var estado_actual = 'A';
 
-    adminFactory.getDatosxACL()
-        .then(function(data) {
-            va.usuarios_acl=data;
-            console.log(va.usuarios_acl);
-            //console.log('va.usuarios_acl');
-        })
-        .catch(function(err) {
-            va.usuarios_acl = {};
-  });
+        va.estadousuario = [
+            {value: 'A', text: 'Activo'},
+            {value: 'C', text: 'Cesado'},   
+        ]; 
 
-}]);
+        va.showEstadoUsuario = function(usuario) {
+            var selected = [];
+            if(usuario.estado) {
+              selected = $filter('filter')(va.estadousuario, {value: usuario.estado});
+            }
+            return selected.length ? selected[0].text : 'Not set';
+        };
+
+        va.cargarUsuarios = function(estado) {
+            listarUsuarios(estado);
+        };
+
+        var listarUsuarios = function(estado) {
+            estado_actual = estado;
+            httpFactory.getUsuarios(estado)
+                .then(function(data) {
+                    va.usuarios = [];
+                    data.forEach(function(item) {
+                        usuario = new adminFactory.Usuario(item.uid, item.estado,
+                        item.areaid,item.nombreestado);
+                        va.usuarios.push(usuario);
+                    });
+                })
+                .catch(function(err) {
+                    va.usuarios= [];
+                });
+        };
+
+        listarUsuarios(estado_actual);
+
+    }
+]);
 
