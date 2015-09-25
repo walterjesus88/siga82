3f2c68fe5ab89b0f08c95e69b0e0799ba725b043
fabio colan <fcolanwong@gmail.com>
2015-09-25 14:00:47 -0500

    avance del modulo rendiciongastos

--- app/modules/rendiciongastos/controllers/IndexController.php
+++ app/modules/rendiciongastos/controllers/IndexController.php
@@ -5,7 +5,7 @@ class RendicionGastos_IndexController extends Zend_Controller_Action {
     public function init() {
     	$options = array(
             'layout' => 'inicio',
-        );
+            );
         Zend_Layout::startMvc($options);
     }
 
@@ -17,11 +17,17 @@ class RendicionGastos_IndexController extends Zend_Controller_Action {
 
       $this->_helper->layout()->disableLayout();
 
-    }
+  }
 
-    public function modalrendicionAction()
-    {
-  $this->_helper->layout()->disableLayout();
-    }
+    public function rendirgastosAction() {
+
+      $this->_helper->layout()->disableLayout();
+
+  }
+
+  public function modalrendicionAction()
+  {
+      $this->_helper->layout()->disableLayout();
+  }
 
 }
--- html/js/RendicionGastos/services/gastosFactory.js
+++ html/js/RendicionGastos/services/gastosFactory.js
@@ -1,34 +1,36 @@
-app.factory('gastosFactory', ['httpFactory', '$location', '$q',
-  function(httpFactory, $location, $q) {
+app.factory('gastosFactory', ['httpFactory', '$q',
+  function(httpFactory, $q) {
 
-    //     var datos = {
-    //     uid: '',
-    //     dni: '',
-    //     numero_completo: '',
-    //     nombre: '',
-    //     fecha: '',
-    //     monto_total: '',
-    //     estado: '',
-    // };
+/*    var datos = {
+      uid: '',
+      dni: '',
+      numero_completo: '',
+      numero:'',
+      nombre: '',
+      fecha: '',
+      monto_total: '',
+      estado: '',
+    };*/
 
     var publico = {
 
-      Gasto: function(numero_completo,nombre,fecha,monto_total,estado) {
+      Gasto: function(item) {
         var estados = {
           'B': 'Pendiente',
           'E': 'Enviado',
           'A': 'Aprobado',
           'R': 'Rechazado'
         }
-        this.numero_completo = numero_completo;
-        this.nombre = nombre;
-        this.fecha = fecha;
-        this.monto_total = monto_total;
-        this.estado = estados[estado];
+        this.numero = item.numero;
+        this.numero_completo = item.numero_completo;
+        this.nombre = item.nombre;
+        this.fecha = item.fecha;
+        this.monto_total = item.monto_total;
+        this.estado = estados[item.estado];
 
             // console.log(this.estado);
             this.cambiarEstadoGasto = function() {
-              httpFactory.setGasto(this.numero_completo,this.nombre,this.fecha,this.monto_total,this.estado)
+              httpFactory.setGasto(this.numero,this.numero_completo,this.nombre,this.fecha,this.monto_total,this.estado)
               .then(function(data) {
                 alert('Estado del gastos cambiado');
               })
@@ -36,27 +38,47 @@ app.factory('gastosFactory', ['httpFactory', '$location', '$q',
                 alert('No se pudo cambiar el estado');
               })
             }
-          },
+    },
 
 
-          setGuardarRendicion: function(numero_completo,nombre,fecha,estado) {
-            var defered = $q.defer();
-            var promise = defered.promise;
+    setGuardarRendicion: function(numero_completo,nombre,fecha,estado) {
+      var defered = $q.defer();
+      var promise = defered.promise;
 
-            httpFactory.setGuardarRendicion(numero_completo,nombre,fecha,estado)
-            .then(function(data) {
-              datos = data;
-              defered.resolve(datos);
+      httpFactory.setGuardarRendicion(numero_completo,nombre,fecha,estado)
+      .then(function(data) {
+        datos = data;
+        defered.resolve(datos);
         // console.log(data);
-        
-
       })
-            .catch(function(err) {
-              defered.reject(err);
-            });
-            return promise;
-          },
-        }
+      .catch(function(err) {
+        defered.reject(err);
+      });
+      return promise;
+    },
 
-        return publico;
-      }]);
+
+    getDatosRendicion: function(numero) {
+      var defered = $q.defer();
+      var promise = defered.promise;
+      httpFactory.getGastosById(numero)
+      .then(function(data) {
+        datos = data;
+        defered.resolve(datos);
+        console.log("gastosFactory");
+      })
+      .catch(function(err) {
+        defered.reject(err);
+      });
+      return promise;
+    },
+
+
+
+
+
+
+  }
+
+  return publico;
+}]);
